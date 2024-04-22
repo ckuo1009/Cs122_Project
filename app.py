54cfb2fd886d15700170233f2ebbc325b1a4e308
Chihkuo Hsu <chih-kuo.hsu@sjsu.edu>
2024-04-21 17:23:15 -0700

    init

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to quiz app!"
+
+if __name__ == '__main__':
+    app.run(debug=True,port=8080)
+
